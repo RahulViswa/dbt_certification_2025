14eabc05d8d64333613e92638412c1b244e6c4c9
Rahul viswanath <rahulchingacham@gmail.com>
2025-10-28 20:04:58 +0530

    dbt fusion configurations

--- macros/generate_custom_database.sql
+++ macros/generate_custom_database.sql
@@ -1,9 +1,16 @@
-{% macro generate_database_name(custom_database_name=none, node=none) -%}
-
+-- macros/generate_database_name.sql
+{% macro generate_database_name(custom_database_name, node) -%}
     {%- set default_database = target.database -%}
-    {%- if target.name == 'ci' -%} {{ env_var('DBT_CI') }}
-    {%- elif custom_database_name is none -%} {{ default_database }}
-    {%- else -%} {{ custom_database_name | trim }}
+    
+    {%- if target.name == 'prod' -%}
+        {%- if custom_database_name is none -%}
+            {{ default_database }}
+        {%- else -%}
+            {{ custom_database_name | trim }}
+        {%- endif -%}
+    {%- elif target.name == 'ci' -%}
+        SANDBOX_CI
+    {%- else -%}
+        SANDBOX_ENGINEERING
     {%- endif -%}
-
 {%- endmacro %}
